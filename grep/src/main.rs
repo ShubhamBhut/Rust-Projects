0e047ecf3d525ea146515b6a9e832262048c35bf
Shubham Patel <shubhamprecious1@gmail.com>
2023-02-14 12:57:33 +0530

    Refactor the code for error handling

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,17 +1,48 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err: &str| {
+        println!("Problem parsing the arguments, {}", err);
+        process::exit(1);
+    });
+   
+    println!("Searching for {}", config.query);
+    println!("The file path is {}", config.file_path);
+    
+    if let Err(e) = run(config){
+        println!("Application Error: {}", e);
+        process::exit(1);
+    }
+    }
 
-    let query = &args[1];
-    let file_path = &args[2];
-
-    println!("Searching for {}", query);
-    println!("The file path is {}", file_path);
-
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-
+fn run(config:Config) -> Result<(), Box<dyn Error>>{
+    
+    let contents = fs::read_to_string(config.file_path)?;
     println!("with text:{}", contents);
+
+    Ok(())
+}
+
+struct Config {
+    query : String,
+    file_path: String
+}
+
+impl Config {
+
+    fn new(args: &[String]) -> Result<Config, &str>{
+
+        if args.len() < 3 {
+            return Err("Not enough arguments!")
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Ok(Config { query: query, file_path: file_path })
+}
 }
