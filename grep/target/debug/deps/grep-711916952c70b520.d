15e1c481e6ddc61ccf4144a78558cf9efbace601
Shubham Patel <shubhamprecious1@gmail.com>
2023-02-14 11:50:54 +0530

    Add input

---
+++ grep/target/debug/deps/grep-711916952c70b520.d
@@ -0,0 +1,5 @@
+/home/kailorston/my_work/rust-projects/grep/target/debug/deps/grep-711916952c70b520: src/main.rs
+
+/home/kailorston/my_work/rust-projects/grep/target/debug/deps/grep-711916952c70b520.d: src/main.rs
+
+src/main.rs:
---
+++ grep/target/debug/deps/grep-ae2e7f0306e82a77.d
@@ -0,0 +1,5 @@
+/home/kailorston/my_work/rust-projects/grep/target/debug/deps/grep-ae2e7f0306e82a77.rmeta: src/main.rs
+
+/home/kailorston/my_work/rust-projects/grep/target/debug/deps/grep-ae2e7f0306e82a77.d: src/main.rs
+
+src/main.rs:
