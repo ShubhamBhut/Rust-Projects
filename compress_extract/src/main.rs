8f9f136b0990ae0a43c1ca461ce375426833661c
Shubham Patel <shubhamprecious1@gmail.com>
2023-02-20 08:49:34 +0530

    Add decompression

--- compress_extract/src/main.rs
+++ compress_extract/src/main.rs
@@ -1,7 +1,7 @@
 extern crate flate2;
 use flate2::read::GzDecoder;
 use flate2::write::GzEncoder;
-use flate2::{Compression, Decompress};
+use flate2::Compression;
 use std::env::args;
 use std::io::copy;
 use std::fs::File;
@@ -15,16 +15,18 @@ fn main() {
         eprintln!("Usage: `compress/decompress` `source` `target`");
         return;
     }
-    
-    if args().nth(1).unwrap() == "compress"{
-        compression();
-    }
-
-    }
-
-fn compression() -> File{
-    let mut input = BufReader::new(File::open(args().nth(2).unwrap()).unwrap());
+    let input = BufReader::new(File::open(args().nth(2).unwrap()).unwrap());
     let output = File::create(args().nth(3).unwrap()).unwrap();
+
+    if args().nth(1).unwrap() == "compress"{
+        compression(input, output);
+    }else if args().nth(1).unwrap() == "decompress"{
+        decompression(input, output);
+    }
+
+    }
+
+fn compression(mut input:BufReader<File>, output:File) -> File{
     let mut encoder = GzEncoder::new(output, Compression::default());
     let starttime = Instant::now();
     copy(&mut input, &mut encoder).unwrap();
@@ -34,3 +36,14 @@ fn compression() -> File{
     println!("Elapsed: {:?}", starttime.elapsed());
     output
 }
+
+fn decompression(mut input: BufReader<File>, mut output: File) -> File {
+    let mut decoder = GzDecoder::new(input.get_mut());
+    let starttime = Instant::now();
+    copy(&mut decoder, &mut output).unwrap();
+    println!("Source len: {}", input.get_ref().metadata().unwrap().len());
+    println!("Target len: {}", output.metadata().unwrap().len());
+    println!("Elapsed: {:?}", starttime.elapsed());
+    output
+}
+
